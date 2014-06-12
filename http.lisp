5aef8f58d218dd53b8f7897afed680fa3d1f6e9e
Mark Simpson <verdammelt@gmail.com>
2014-06-11 21:22:34 -0400

    Doing some cleanup.

--- http.lisp
+++ http.lisp
@@ -5,35 +5,27 @@
 (in-package :HTTPL)
 
 (defun read-from-socket (socket)
-  ;; (let ((response (multiple-value-list (socket-receive socket nil 10))))
-  ;;   (format t "~A" response)
-  ;;   (first response))
-  (apply #'concatenate 'string 
-	 (loop for response = (multiple-value-list (socket-receive socket nil 10 :dontwait t))
-	    collecting (first response)
-	    until (or (not (second response)) (= (second response) 0))))
-
-  ;; (multiple-value-bind (buffer length)
-  ;;     (socket-receive socket nil 10)
-  ;;   (subseq buffer 0 length))
-  )
+  (let ((buffer-size (* 10 1024)))
+   (multiple-value-bind (buffer length)
+       (socket-receive socket nil buffer-size)
+     (subseq buffer 0 length))))
 
 (defun respond (socket response)
   (let ((payload (concatenate 'string response '(#\Return #\Linefeed))))
     (socket-send socket payload nil)))
+
+(defun process-request (socket)
+  (unwind-protect
+       (progn 
+	 (format t  "-> ~A~%" (read-from-socket socket))
+	 (respond socket "HTTP/1.1 200 OK"))
+    (socket-close socket)))
 
 (defun server (&optional (port 8080))
   (let ((socket (make-instance 'inet-socket :type :stream :protocol :tcp)))
     (unwind-protect
 	 (progn
 	   (socket-bind socket #(127 0 0 1) port)
-	   (socket-listen socket 10)	; good value for backlog parameter?
-	   (loop do (let ((client-socket (socket-accept socket)))
-		      (unwind-protect
-		       (progn 
-			 (format t  "-> ~A~%" (read-from-socket client-socket))
-			 (respond client-socket "HTTP/1.1 200 OK")
-			 (socket-close client-socket))
-		       (socket-close client-socket)))))
-	 (socket-close socket))
-    ))
+	   (socket-listen socket 5)
+	   (loop (process-request (socket-accept socket))))
+      (socket-close socket)))))
